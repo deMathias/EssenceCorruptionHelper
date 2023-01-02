91f247e27ecdb2bba0f4f0e0176256602b4f86a2
deMathias <47175867+deMathias@users.noreply.github.com>
2023-01-02 13:24:23 +0100

    settings commit

--- EssenceCorruptionHelper.cs
+++ EssenceCorruptionHelper.cs
@@ -1,6 +1,21 @@
-﻿namespace EssenceCorruptionHelper;
+﻿using DefaultNamespace;
+using ExileCore;
+using ExileCore.PoEMemory.MemoryObjects;
+using SharpDX;
 
-public class EssenceCorruptionHelper
+namespace EssenceCorruptionHelper;
+
+public class EssenceCorruptionHelper : BaseSettingsPlugin<Settings>
 {
+    private const double CameraAngle = 38.7 * Math.PI / 180;
+    private static readonly float CameraAngleCos = (float)Math.Cos(CameraAngle);
+    private static readonly float CameraAngleSin = (float)Math.Sin(CameraAngle);
+    private const float GridToWorldMultiplier = 250 / 23f;
+    private double _mapScale;
+    private Vector2 _mapCenter;
+    private bool _largeMapOpen;
     
+    private Camera Camera => GameController.Game.IngameState.Camera;
+    
+    private void Draw
 }
---
+++ Settings.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json.Serialization;
+using ExileCore.Shared.Attributes;
+using ExileCore.Shared.Interfaces;
+using ExileCore.Shared.Nodes;
+
+namespace DefaultNamespace;
+
+public class Settings : ISettings
+{
+    public Settings()
+    {
+        
+    }
+    
+    public ToggleNode Enable { get; set; } = new ToggleNode(false);
+
+    [Menu("6+ Essence?")]
+    [JsonIgnore] 
+    public ToggleNode SettingsOnCount { get; set; } = new ToggleNode(true);
+
+    [Menu("MEDS? (Misery or Envy or Dread or Scorn)")]
+    [JsonIgnore]
+    public ToggleNode SettingsOnMeds { get; set; } = new ToggleNode(true);
+
+}

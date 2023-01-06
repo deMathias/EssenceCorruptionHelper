153fe8423a4baea9d205990af2449e3bf335083c
deMathias <47175867+deMathias@users.noreply.github.com>
2023-01-06 14:38:38 +0100

    Meds properly detected.

--- EssenceCorruptionHelper.cs
+++ EssenceCorruptionHelper.cs
@@ -7,6 +7,7 @@ using ExileCore.PoEMemory.MemoryObjects;
 using ExileCore.Shared.Enums;
 using ExileCore.Shared.Helpers;
 using SharpDX;
+using static System.Net.Mime.MediaTypeNames;
 using Color = SharpDX.Color;
 using RectangleF = SharpDX.RectangleF;
 using Vector2 = System.Numerics.Vector2;
@@ -16,7 +17,6 @@ namespace EssenceCorruptionHelper;
 public class EssenceCorruptionHelper : BaseSettingsPlugin<Settings>
 {
     private IngameState ingameState;
-    private List<LabelOnGround> listGroundLabels;
 
     public override bool Initialise()
     {
@@ -31,16 +31,43 @@ public class EssenceCorruptionHelper : BaseSettingsPlugin<Settings>
         Draw();
     }
 
-    private void FilterLabels()
+    bool MonolithHasMeds()
     {
-        listGroundLabels = GameController.IngameState.IngameUi.ItemsOnGroundLabelsVisible
-                .Where(x => x.ItemOnGround.Path == "Metadata/MiscellaneousObjects/Monolith").ToList();
-    }
+        Func<string, bool> shouldHighlight = (text) => {
+            if (string.IsNullOrEmpty(text)) return false;
+            if (text.Contains("Misery"))
+            {
+                return true;
+            }
+            if (text.Contains("Envy"))
+            {
+                return true;
+            }
+            if (text.Contains("Dread"))
+            {
+                return true;
+            }
+            if (text.Contains("Scorn"))
+            {
+                return true;
+            }
+            /*if (text.Contains("Greed"))
+            {
+                return true;
+            }
+            */
+            
 
-    private bool MonolithHasMeds()
-    {
-        //check if there is Misery, Envy, Dread or Scorn Essence.
-        return true;
+            // No matches, return false
+            return false;
+        };
+
+        return GameController.IngameState.IngameUi.ItemsOnGroundLabelsVisible
+                    .Where(x => x.ItemOnGround.Path == "Metadata/MiscellaneousObjects/Monolith")
+                    .First().Label.Children[1].Children
+                    .Select(x => x.Text)
+                    // Returns true if any of the properties text 
+                    .Any(t => shouldHighlight(t));
     }
 
     private bool MonolithHasSixOrMoreEssence()
@@ -51,14 +78,36 @@ public class EssenceCorruptionHelper : BaseSettingsPlugin<Settings>
 
     private void Draw()
     {
+        //add drawing.
+        var camera = ingameState.Camera;
+        var playerPos = GameController.Player.PosNum;
 
-    }
-    
-    private void DisplayLabel(Vector2 pos, string text)
-    {
-        pos.Y += 20; // Offset the label slightly so it's not on top of the monolith
-        pos.X += 20; // Offset the label horizontally
+        var entities = GameController.Entities
+            .Where(entity => entity.HasComponent<Render>() &&
+                             entity.Address != GameController.Player.Address &&
+                             entity.IsValid &&
+                             entity.IsTargetable &&
+                             entity.HasComponent<Monolith>());
 
-        Graphics.DrawText(text,pos,Color.Red);
+        foreach (var entity in entities)
+        {
+            var entityPos = entity.PosNum;
+            var entityScreenPos = camera.WorldToScreen(entityPos.Translate(0, 0, 0));
+            var playerScreenPos = camera.WorldToScreen(playerPos.Translate(0, 0, 0));
+
+            if (Settings.ToggleMeds)
+            {
+                //highlight MEDS Shrine
+                if (MonolithHasMeds())
+                {
+                    Graphics.DrawText("Corrupt me", entityScreenPos, Color.Red, FontAlign.Center);
+                }
+            }
+
+            if (Settings.ToggleSixEssence)
+            {
+                
+            }
+        }
     }
 }
